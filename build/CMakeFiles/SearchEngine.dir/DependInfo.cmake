36b2c4f9b97f7103efe6cb66bb7c5951a26023a8
ncourtel <courtel.nattan19@gmail.com>
2024-11-17 17:16:14 +0100

    index & search features

---
+++ build/CMakeFiles/SearchEngine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nattan/Documents/cs/personal-projects/search-engine/src/Indexer.cpp" "CMakeFiles/SearchEngine.dir/src/Indexer.cpp.o" "gcc" "CMakeFiles/SearchEngine.dir/src/Indexer.cpp.o.d"
+  "/Users/nattan/Documents/cs/personal-projects/search-engine/src/Searcher.cpp" "CMakeFiles/SearchEngine.dir/src/Searcher.cpp.o" "gcc" "CMakeFiles/SearchEngine.dir/src/Searcher.cpp.o.d"
+  "/Users/nattan/Documents/cs/personal-projects/search-engine/src/main.cpp" "CMakeFiles/SearchEngine.dir/src/main.cpp.o" "gcc" "CMakeFiles/SearchEngine.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SearchEngine.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SearchEngine.dir/src/Indexer.cpp.o"
+  "CMakeFiles/SearchEngine.dir/src/Indexer.cpp.o.d"
+  "CMakeFiles/SearchEngine.dir/src/Searcher.cpp.o"
+  "CMakeFiles/SearchEngine.dir/src/Searcher.cpp.o.d"
+  "CMakeFiles/SearchEngine.dir/src/main.cpp.o"
+  "CMakeFiles/SearchEngine.dir/src/main.cpp.o.d"
+  "SearchEngine"
+  "SearchEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SearchEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
