dab4197674665d3fb10da33afb3b4c48242e520d
ncourtel <courtel.nattan19@gmail.com>
2024-11-18 11:06:46 +0100

    update function

--- include/searcher.h
+++ include/searcher.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 
-std::vector<std::string> SearchWord(const std::unordered_map<std::string, std::unordered_map<std::string, int>>& dataBase, const std::string& word);
 const std::string UserInterface();
-void displayFileContent(const std::vector<std::string>& files);
+std::unordered_map<std::string, std::vector<int>> SearchStr(const std::vector<std::string>& files, const std::string& str);
+void DisplayLineContent(std::unordered_map<std::string, std::vector<int>> fileAndLines);
+void displayFileContent(const std::string& file);
